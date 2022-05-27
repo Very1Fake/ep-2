84b0a904b92ff880cb47b24f4ce5e59e3b8eb156
very1fake <very1fake.coder@gmail.com>
2022-05-27 16:50:13 +0300

    feat: add dummy second_variant API route

--- routes.py
+++ routes.py
@@ -1,6 +1,43 @@
-from bottle import route, view
+import math
 
-@route('/')
+from bottle import post, request, view, get
+
+@get('/')
 @view('index')
 def index():
     return
+
+@post('/api/second_variant')
+def second_variant_api():
+    from time import sleep
+    import random  # FIX
+
+    # TODO Make validations
+    t1 = float(request.forms.get('t1'))
+    t2 = float(request.forms.get('t2'))
+    a = int(request.forms.get('a'))
+
+    rows = []
+    total = 0
+
+    for row in range(a):
+        current = random.random()
+
+        rows.append([
+            row + 1,
+            round(random.random(), 2),
+            round(random.random(), 2),
+            round(total - current, 3),
+            round(total, 3),
+            round(total + t1, 3),
+            '',
+            '',
+            '',
+            '',
+        ])
+
+        total += current
+
+    sleep(.5)
+
+    return {'ok': rows}
