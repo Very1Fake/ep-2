d3fb723f5c3b8bb353a593b95df9bd710478c905
very1fake <very1fake.coder@gmail.com>
2022-05-29 13:36:35 +0300

    feat: add dummy third_variant API route

--- routes.py
+++ routes.py
@@ -2,24 +2,28 @@ import math
 
 from bottle import post, request, view, get
 
+
 @get('/')
 @view('index')
 def index():
     return
+
 
 @get('/second_variant')
 @view('second_variant')
 def second_variant():
     return
 
+
 @get('/first_variant')
 @view('first_variant')
 def first_variant():
     return
 
+
 @post('/api/first_variant')
 def first_variant_api():
-    
+
     import random  # FIX
 
     # TODO Make validations
@@ -30,8 +34,8 @@ def first_variant_api():
     E = float(request.forms.get('numE'))
     rowCount = int(request.forms.get('rowCount'))
 
-
     return {'ok': rows}
+
 
 @post('/api/second_variant')
 def second_variant_api():
@@ -56,10 +60,47 @@ def second_variant_api():
             round(total - current, 3),
             round(total, 3),
             round(total + t1, 3),
-            '',
-            '',
-            '',
-            '',
+            '2',
+            '3',
+            '1',
+            '2',
+        ])
+
+        total += current
+
+    sleep(.5)
+
+    return {'ok': rows}
+
+
+@post('/api/third_variant')
+def third_variant_api():
+    from time import sleep
+    import random  # FIX
+
+    # TODO Make validations
+    t1 = float(request.forms.get('t1'))
+    t2 = float(request.forms.get('t2'))
+    a = int(request.forms.get('a'))
+
+    rows = []
+    total = 0
+
+    for row in range(a):
+        current = random.random()
+
+        rows.append([
+            row + 1,
+            round(random.random(), 2),
+            round(random.random(), 2),
+            round(total - current, 3),
+            round(total, 3),
+            round(total + t1, 3),
+            '2',
+            '3',
+            '4',
+            '1',
+            '2',
         ])
 
         total += current
